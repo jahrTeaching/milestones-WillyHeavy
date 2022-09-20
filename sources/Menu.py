2966bd91fe7e398f6ee07b07e3ab44ed74cc1595
GuillermoGdR <Guillermo.gdelrio@gmail.com>
2022-09-20 18:53:45 +0200

    Código de Crank-Nicolson añadido
    He añadido el código de C-N. Para despejar el sistema he usado una función de scipy, espero que esté permitido.

--- sources/Menu.py
+++ sources/Menu.py
@@ -1,6 +1,8 @@
 
+
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import fsolve
 
 print('Por favor, defina las condiciones iniciales del problema en el siguiente orden: X0, Y0, Vx0, Vy0')
 
@@ -63,6 +65,30 @@ if choice == 1:
 elif choice == 2:
 
     print('Has elegido Crank')
+
+    F0 = Fs(X0,Y0,Vx0,Vy0)
+    F  = F0
+    U  = U0
+    def sistema(Un1):
+
+        x,y,z,s = Un1
+        return(x - U[0] - dt/2*U[2] - dt/2*z,
+               y - U[1] - dt/2*U[3] - dt/2*s, 
+               z - U[2] -dt/2*(-U[0]/(U[0]**2 + U[1]**2)**(3/2)) - dt/2*(-x/(x**2 + y**2)**(3/2)),
+               s - U[3] -dt/2*(-U[1]/(U[0]**2 + U[1]**2)**(3/2)) - dt/2*(-y/(x**2 + y**2)**(3/2)))
+ 
+
+    for i in range(1,n+1):
+
+        x,y,z,s = fsolve(sistema, (U[0],U[1],U[2],U[3]))
+
+        U = np.array([x,y,z,s])
+
+        Xplot = [X0] + [U[0]]
+        Yplot = [Y0] + [U[1]]
+        plt.plot(Xplot,Yplot,'bo')
+
+
 
 
 elif choice == 3:
