f4dbfb6ca7446bd4ea68adf0f82ae2091057c4cd
GuillermoGdR <Guillermo.gdelrio@gmail.com>
2022-10-11 17:30:06 +0200

    Update Hito 3

--- sources/Hito 3.py
+++ sources/Hito 3.py
@@ -1,9 +1,10 @@
 ## HITO 3 ## 
 
-from numpy import array, linspace
+from numpy import array, linspace, zeros 
 from Numeric.Esquemas_numéricos import Euler,RK4, Crank_Nicolson, Euler_inverso
+from Numeric.Error import Richardson, Temporal_convergence_rate
 from Physics.Órbitas import Kepler
-from Mathematics.EDOS import Cauchy_Problem
+from math import log10
 import matplotlib.pyplot as plt
 
 ## Variables temporales ##
@@ -13,11 +14,50 @@ dt = 0.001                        # Paso de integráción en segundos
 n = int(T/dt)                    # Numero de pasos 
 t  = linspace(0,T,n)             # Vector de instantes separados dt
 
+t1 = t
+t2 = linspace(0,T,2*n)
+
 ## Condiciones iniciales ##
 
 U0 = array([1,0,0,1])
-U = U0
 
+log_N = zeros(n)
+
+for i in range (n):
+
+    log_N[i] = log10(n)
+
+methods = [Euler,RK4, Crank_Nicolson, Euler_inverso]
+lista = ['Euler','RK4','Crank Nicolson','Euler inverso']
+
+for j in range (4): 
+
+    Error = Richardson(Kepler, methods[j], t, U0)
+    Error_norm = (Error[:,0]**2 + Error[:,1]**2)**(1/2)
+
+    plt.figure(1)
+    plt.subplot(211)
+    plt.title(f'Error de {lista[j]} con dt = {dt} s y T = {T} s')
+    plt.plot(t, Error[:,0],"r", label = "X position")
+    plt.plot(t, Error[:,1],"b", label = "Y position")
+    plt.xlabel("t")
+    plt.ylabel("Error")
+    plt.legend(loc = "lower left")
+    plt.subplot(212)
+    plt.plot(t, Error_norm, 'c', label = 'Module')
+    plt.xlabel("t")
+    plt.ylabel("Error")
+    plt.legend(loc ='lower right')
+    plt.show()
+
+    Convergence = Temporal_convergence_rate(Kepler, methods[j], t, U0)
+    
+    
+    plt.plot(Convergence)
+    plt.show()
+
+
+'''
 methods = [Euler,RK4, Crank_Nicolson, Euler_inverso]
 lista = ['Euler','RK4','Crank Nicolson','Euler inverso']
 
@@ -36,3 +76,4 @@ for j in range (4):
 
     plt.show()
 
+'''
---
+++ sources/Numeric/Error.py
@@ -0,0 +1,76 @@
+import sys
+sys.path.append('.')
+
+from numpy import linspace, zeros, log10, round_
+from numpy.linalg import norm
+from Mathematics.EDOS import Cauchy_Problem
+from Numeric.Esquemas_numéricos import Euler, RK4, Crank_Nicolson, Euler_inverso
+
+
+
+def Richardson(Problem,Scheme, t, U0):
+
+    T  = t[-1]
+    n  = len(t)
+    t1 = t
+    t2 = linspace(0,T,2*n)
+    
+    U1 = Cauchy_Problem(Problem, t1, U0, Scheme)
+    U2 = Cauchy_Problem (Problem, t2, U0, Scheme )
+
+    Error = zeros((n,len(U0)))
+    
+    if Scheme == Euler:
+        q = 1
+    elif Scheme == RK4:
+        q = 4
+    elif Scheme == Crank_Nicolson:
+        q = 2
+    elif Scheme == Euler_inverso:
+        q = 1
+
+    for i in range(n):
+        Error[i,:] = (U2[2*i,:] - U1[i,:])/(1-1/(2**q))
+
+    return Error 
+
+
+def Temporal_convergence_rate(Problem, Scheme, t, U0):  
+    
+    T  = t[-1]
+    n  = len(t)
+    t1 = t
+    t2 = linspace(0,T,2*n)
+    
+    U1 = Cauchy_Problem(Problem, t1, U0, Scheme)
+    U2 = Cauchy_Problem (Problem, t2, U0, Scheme )
+    
+    m = 20 
+    log_E = zeros(m)
+    log_N = zeros(m) 
+ 
+    for i in range (0,m):
+
+        Error = norm(U2[2*i,:] - U1[i,:])
+        log_E[i] = log10(Error)
+        log_N[i] = log10(2*n)
+
+    for j in range(0,n):
+
+         if (abs(log_E[j]) > 12):
+
+             break
+
+    j = min(j, m)
+    """reg = LinearRegression().fit(log_N[0:j+1].reshape((-1, 1)),log_E[0:j+1])
+    order = round_(abs(reg.coef_),1)
+
+    log_N_lineal = log_N[0:j+1]
+    log_E_lineal = reg.predict(log_N[0:j+1].reshape((-1, 1)))"""
+
+    return [log_N, log_E]
+   
+   
+    
+    
+
